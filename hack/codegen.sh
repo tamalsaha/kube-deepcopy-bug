b0ca7989986ea7a803c9a2641c7ecd75a7bb91a4
tamal <tamal@appscode.com>
2018-04-22 07:00:41 -0700

    Vendor code-generator

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -10,15 +10,20 @@ DOCKER_CODEGEN_PKG="/go/src/k8s.io/code-generator"
 
 pushd $REPO_ROOT
 
-docker run --rm -ti -u $(id -u):$(id -g) \
-  -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
-  -w "$DOCKER_REPO_ROOT" \
-  appscode/gengo:release-1.9 "$DOCKER_CODEGEN_PKG"/generate-groups.sh "deepcopy" \
+./vendor/k8s.io/code-generator/generate-groups.sh "deepcopy" \
   github.com/tamalsaha/kube-deepcopy-bug/client \
   github.com/tamalsaha/kube-deepcopy-bug/apis \
   demo:v1alpha1 \
-  --go-header-file "$DOCKER_REPO_ROOT/hack/gengo/boilerplate.go.txt"
+  --go-header-file "$REPO_ROOT/hack/gengo/boilerplate.go.txt"
 
+# docker run --rm -ti -u $(id -u):$(id -g) \
+#   -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
+#   -w "$DOCKER_REPO_ROOT" \
+#   appscode/gengo:release-1.9 "$DOCKER_CODEGEN_PKG"/generate-groups.sh "deepcopy" \
+#   github.com/tamalsaha/kube-deepcopy-bug/client \
+#   github.com/tamalsaha/kube-deepcopy-bug/apis \
+#   demo:v1alpha1 \
+#   --go-header-file "$DOCKER_REPO_ROOT/hack/gengo/boilerplate.go.txt"
 
 go build ./...
 
